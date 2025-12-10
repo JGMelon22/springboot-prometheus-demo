f824c3d45e4ebea3bd9fb851821ce620f464c9fe
JGMelon22 <jgvasconcellos22@gmail.com>
2025-12-10 20:45:09 -0300

    chore: update docker file to use Java 21, cleanup Prometheus and Grafana setup

--- src/main/java/com/jgmelon22/prometheus_example/controller/UserController.java
+++ src/main/java/com/jgmelon22/prometheus_example/controller/UserController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.jgmelon22.prometheus_example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;

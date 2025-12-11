9ecb398372c474f05e81d118e8c941d78607be0d
JGMelon22 <jgvasconcellos22@gmail.com>
2025-12-10 21:03:56 -0300

    feat: develop product service and product controller

---
+++ src/main/java/com/jgmelon22/prometheus_example/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.jgmelon22.prometheus_example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jgmelon22.prometheus_example.dto.ProductDto;
+import com.jgmelon22.prometheus_example.model.Product;
+import com.jgmelon22.prometheus_example.service.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public Iterable<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody ProductDto productDetails) {
+        return ResponseEntity.ok(productService.updateProduct(id, productDetails));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/jgmelon22/prometheus_example/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.jgmelon22.prometheus_example.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jgmelon22.prometheus_example.dto.ProductDto;
+import com.jgmelon22.prometheus_example.model.Product;
+import com.jgmelon22.prometheus_example.repository.ProductRepository;
+
+public class ProductService {
+ @Autowired
+    private ProductRepository productRepository;
+
+    public Iterable<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, ProductDto productDetails) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+        BeanUtils.copyProperties(productDetails, product); 
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+        productRepository.delete(product);
+    }
+}
